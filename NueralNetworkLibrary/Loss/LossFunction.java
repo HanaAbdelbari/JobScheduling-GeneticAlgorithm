c168e389867959c733946f43a7d6bab947782ccd
HanaAbdelbari <hanaabdelbari882@gmail.com>
2025-12-13 22:34:57 +0200

    Loss package done

--- NueralNetworkLibrary/Loss/LossFunction.java
+++ NueralNetworkLibrary/Loss/LossFunction.java
@@ -1,4 +1,10 @@
 package NueralNetworkLibrary.Loss;
 
 public interface LossFunction {
+
+    double loss(double[][] yTrue, double[][] yPred);
+
+    //returns Gradient
+    double[][] derivative(double[][] yTrue, double[][] yPred);
+
 }
