a9b58d6e63d65fb510666039f4d52b95198e467a
HanaAbdelbari <hanaabdelbari882@gmail.com>
2025-12-15 22:07:33 +0200

    Initialization of weights with three methods

--- NueralNetworkLibrary/Initialization/XavierInit.java
+++ NueralNetworkLibrary/Initialization/XavierInit.java
@@ -1,4 +1,33 @@
 package NueralNetworkLibrary.Initialization;
 
-public class XavierInit implements Initializer{
+import java.util.Random;
+
+
+ // Best for Sigmoid and Tanh activation functions
+
+public class XavierInit implements Initializer {
+    private Random random;
+
+    public XavierInit() {
+        this.random = new Random();
+    }
+
+    public XavierInit(long seed) {
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public double[][] initialize(int nInputs, int nNeurons) {
+        // Xavier formula
+        double limit = Math.sqrt(6.0 / (nInputs + nNeurons));
+
+        double[][] weights = new double[nInputs][nNeurons];
+        for (int i = 0; i < nInputs; i++) {
+            for (int j = 0; j < nNeurons; j++) {
+                // Uniform distribution in [-limit, +limit]
+                weights[i][j] = -limit + 2 * limit * random.nextDouble();
+            }
+        }
+        return weights;
+    }
 }
